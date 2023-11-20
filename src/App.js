e13ebd699ebfb1b33c650e89329899168aad8f56
DESKTOP-9GRKLB4\Dao Uyen <uyendaothi@bizfly.vn>
2023-11-20 17:46:54 +0700

    useState Hook

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
 import Nav from './views/Nav';
+import { useState } from 'react';
 
-function App() {
+
+const App = () => {
+
+  let [name, setName] = useState('Uyên');
+  const [address, setAddress] = useState('');
+  const handleEventClick = () => {
+    setName(address);
+    console.log('sffd', name);
+  }
+  const handleOnChange = (event) => {
+    setAddress(event.target.value);
+    console.log('fdfdg', event.target.value)
+  }
+
   return (
     <div className="App">
       <Nav />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello world hh</h1>
+        <h1>Hello world {name}</h1>
+        <input type="text" value={address} onChange={(event) => handleOnChange(event)} />
+        <button type="button" onClick={() => handleEventClick()}>Click</button>
       </header>
     </div>
   );
--- src/views/Nav.js
+++ src/views/Nav.js
@@ -3,9 +3,9 @@ import './Nav.scss';
 const Nav = () => {
     return (
         <div className="topnav">
-            <a href="" className="active">Home</a>
-            <a href="" >About</a>
-            <a href="" >Todo</a>
+            <a href="#" className="active">Home</a>
+            <a href="#" >About</a>
+            <a href="#" >Todo</a>
         </div>
     )
 }
