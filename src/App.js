9f7081f429a9f4fd5425b0447763fc1f0ebfb12b
DESKTOP-9GRKLB4\Dao Uyen <uyendaothi@bizfly.vn>
2023-11-22 08:50:53 +0700

    outputting lists

--- src/App.js
+++ src/App.js
@@ -8,9 +8,19 @@ const App = () => {
 
   let [name, setName] = useState('Uyên');
   const [address, setAddress] = useState('');
+  const [todos, setTodos] = useState([
+    { id: 1, title: 'Todo 1' },
+    { id: 2, title: 'Todo 2' }
+  ]);
   const handleEventClick = () => {
-    setName(address);
-    console.log('sffd', name);
+    if (!address) {
+      alert('wanring');
+      return;
+    }
+    //hook not merge state
+    let todo = { id: 3, title: address }
+    setTodos([...todos, todo]);
+    setAddress('');
   }
   const handleOnChange = (event) => {
     setAddress(event.target.value);
@@ -23,6 +33,15 @@ const App = () => {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Hello world {name}</h1>
+        <div className='todo-container'>
+          {todos.map((item, index) => {
+            //cần thêm key để react thêm hiệu năng
+            return (
+              <li key={item.id} className='todo-child'>{item.title}</li>
+            )
+          })
+          }
+        </div>
         <input type="text" value={address} onChange={(event) => handleOnChange(event)} />
         <button type="button" onClick={() => handleEventClick()}>Click</button>
       </header>
